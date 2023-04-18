55201f54b362aa2eb51051d0adb0111619f68de3
christian20691 <110182640+christian20691@users.noreply.github.com>
2023-04-18 17:41:06 -0300

    Update dynamodb_table.py

--- url_shortener_constructs/dynamodb_table.py
+++ url_shortener_constructs/dynamodb_table.py
@@ -2,8 +2,6 @@ from aws_cdk import aws_dynamodb
 import aws_cdk
 from constructs import Construct
 
-# partition_key=dynamodb.Attribute(name="id", type=dynamodb.AttributeType.STRING),
-
 class dynamodDBProps(dict):
     table_name: str
     partition_key: aws_dynamodb.Attribute
