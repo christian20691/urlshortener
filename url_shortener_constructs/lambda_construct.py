404f996ee746902fe0150ce31abb20850fa493dc
christian20691 <110182640+christian20691@users.noreply.github.com>
2023-04-18 17:40:47 -0300

    Update lambda_construct.py

--- url_shortener_constructs/lambda_construct.py
+++ url_shortener_constructs/lambda_construct.py
@@ -2,20 +2,6 @@ import aws_cdk
 from aws_cdk import aws_lambda
 from constructs import Construct
 
-# id=experience_api_lambda_name,
-# function_name=experience_api_lambda_name,
-# description="Experiences API Lambda",
-# runtime=aws_lambda.Runtime.PYTHON_3_9,
-# timeout=lambda_timeout_seconds,
-# code=aws_lambda.Code.from_asset(f"{stack_path}/bazel-bin/app/src/data_service/experience_api/lambda_archive.zip"),
-# handler="lambda_function.lambda_handler",
-# environment=typing.cast(typing.Dict[str, str], lambda_environment),
-# allow_public_subnet=False,
-
-## Code
-#lambda_code = aws_lambda.Code.from_asset(os.path.join("/lambda_code/", "lambda_handler.py"))
-
-
 class lambdaProps(dict):
     function_name: str
     description: str
